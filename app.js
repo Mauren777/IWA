69c3a55e7451c78ff6ab3b92a56d13a175e9f491
Mauren777 <maura.tangara@gmail.com>
2019-11-12 16:35:14 +0000

    Adds index.html

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile(__dirname + '/public/index.html')
 })
  
 app.listen(port, hostname, () => {
