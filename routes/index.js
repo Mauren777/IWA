f8dfe82c99ad910beec97b68064f6e7cf3862fdd
Mauren777 <maura.tangara@gmail.com>
2019-11-12 18:19:16 +0000

    styles update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LosViajesdeMaura' });
+  res.render('index', { 
+      title: 'LosViajesdeMaura',
+      intro: 'I am <strong>Traveler</strong> &amp; <strong>Blogger</strong> from Bolivia, Who Loves Documenting Adventures &amp; Discoveries Around the World'
+      });
 });
 
 module.exports = router;
