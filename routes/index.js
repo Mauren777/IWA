7985c7d3baafa603b7daef42034091483ae0f08e
Mauren777 <maura.tangara@gmail.com>
2019-11-13 12:27:12 +0000

    Installs xml2js package

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express      = require('express');
+var router       = express.Router();
+var fs           = require('fs');
+var xml2js       = require('xml2js');
+var parser       = new xml2js.Parser();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-      title: 'LosViajesdeMaura',
-      intro: 'I am <strong>Traveler</strong> &amp; <strong>Blogger</strong> from Bolivia, Who Loves Documenting Adventures &amp; Discoveries Around the World'
-      });
+  
+  var xmlfile = __dirname + "/../xml/content.xml";
+
+  fs.readFile(xmlfile, "utf-8", function (error, text) {
+        if (error) {
+            throw error;
+        }else {
+            parser.parseString(text, function (err, result) {
+                var articles = result['content']['article'];
+                res.render('index', { 
+                  title: 'LosViajesdeMaura',
+                  intro: 'I am <strong>Traveler</strong> &amp; <strong>Blogger</strong> from Bolivia, Who Loves Documenting Adventures &amp; Discoveries Around the World',
+                  articles:  articles 
+                });
+            });
+        }
+    });
 });
 
 module.exports = router;
